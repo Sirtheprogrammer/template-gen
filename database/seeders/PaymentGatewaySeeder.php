412fe128823ec2b389a51a07f16fffbea39a15c6
sirtheprogrammer <sirtheprogrammer@gmail.com>
2026-03-11 01:59:13 +0300

    ...............................

--- database/seeders/PaymentGatewaySeeder.php
+++ database/seeders/PaymentGatewaySeeder.php
@@ -20,7 +20,7 @@ class PaymentGatewaySeeder extends Seeder
         PaymentGateway::create([
             'name' => 'sonicpesa',
             'display_name' => 'SonicPesa',
-            'api_key' => env('SONICPESA_API_KEY', 'sk_live_TU7Q0bYOQT5rC4zhOPB3JZRAvtJB82tKczIkhfVc'),
+            'api_key' => env('SONICPESA_API_KEY', 'sk_.........................................'),
             'is_active' => true,
             'description' => 'SonicPesa Payment Gateway - USSD payments for Tanzania',
         ]);
@@ -29,7 +29,7 @@ class PaymentGatewaySeeder extends Seeder
         PaymentGateway::create([
             'name' => 'snippe',
             'display_name' => 'Snippe',
-            'api_key' => env('SNIPPE_API_KEY', 'snp_f5e1464da54af60cc99e179592ed55642d769727152ae7a1ba7834c4b4c26c28'),
+            'api_key' => env('SNIPPE_API_KEY', 'snp_.......................................'),
             'webhook_url' => env('SNIPPE_WEBHOOK_URL', 'https://example.com/webhook'),
             'is_active' => false,
             'description' => 'Snippe Payment Gateway - Mobile money payments',
